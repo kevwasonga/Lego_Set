f34c36de7b7202da64074515fb8728c6acebf9cc
kevwasonga <kevinwasonga116@gmail.com>
2025-08-22 01:10:43 +0000

    chore(mobs): add mod.rs to declare mob, boss, member, and actions modules

---
+++ src/mobs/mod.rs
@@ -0,0 +1,14 @@
+pub mod mob;
+pub mod boss;
+pub mod member;
+pub mod actions;
+
+pub use mob::Mob;
+pub use boss::Boss;
+pub use member::Member;
+
+
+// The pub keyword makes the module public, which means this module and its public contents can be accessed from outside the current module or crate (depending on the crate hierarchy).
+
+
+//What are these crates then?
