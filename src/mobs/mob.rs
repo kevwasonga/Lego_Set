cc1535ac304eafa6b425183869511c21e1274ba4
kevwasonga <kevinwasonga116@gmail.com>
2025-08-23 19:23:23 +0000

    refactor: Replace default city with empty cities vec, initialized as vec\![].NEXT TASK... Add method add_city(name: &str, influence: u8) for dynamic city addition

--- src/mobs/mob.rs
+++ src/mobs/mob.rs
@@ -3,24 +3,30 @@ use super::{Boss, Member};
 #[derive(Debug, Clone)]
 pub struct Mob {
     pub name: String,
-    pub boss: Boss,// #debug by turning  boss to a vec
+    pub boss: Boss,// only one boss, so no need for name.
     pub members: Vec<Member>,
     pub cities: Vec<(String, u8)>,
     pub wealth: u32,
     pub allies: Vec<String>,
     pub enemies: Vec<String>,
 }
-//understanding  vecs, and how tey are differently used in the examples above,  why  is it that only these variants are of Vec type
+// 👉 “The struct defines the mob’s shape. The `new` method is a convenience constructor that helps us build mobs with sensible defaults.”
+
 impl Mob {
     pub fn new(name: &str, boss: Boss, wealth: u32, city: &str) -> Self {
         Self {
             name: name.to_string(),
             boss,
             members: vec![],
-            cities: vec![(city.to_string(), 5)],
+            cities: vec![],
             wealth,
             allies: vec![],
             enemies: vec![],
         }
     }
 }
+
+ //CITIES CREATION
+cities: vec![(city.to_string(), 5)],
+* Initialize with `vec![]` (no cities).
+* Provide a method like `add_city(name: &str, influence: u8)`.
